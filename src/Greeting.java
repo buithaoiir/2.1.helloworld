d5bc627afa26d5661157141368351de71f02531b
thaobui <buithaoiir@gmail.com>
2018-07-25 18:11:14 +0700

    try to sout something

---
+++ src/Greeting.java
@@ -0,0 +1,8 @@
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print("world");
+        System.out.print("!");
+        System.out.println();
+    }
+}
